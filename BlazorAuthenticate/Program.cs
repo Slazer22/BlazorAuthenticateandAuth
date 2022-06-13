f82c247cd8a1333ecdffbc5f5b1bd2fefcce4b7f
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-06-13 12:04:42 -0500

    Add project files.

---
+++ BlazorAuthenticate/Program.cs
@@ -0,0 +1,34 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+using BlazorAuthenticate;
+using BlazorAuthenticate.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
